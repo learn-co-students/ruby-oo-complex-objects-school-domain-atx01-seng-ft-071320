8bda646c43a7e750be951e9d27b4f0bef9e2649a
R-Tomas-Gonzalez <tomas@rtomasgonzalez.com>
2020-07-15 18:24:26 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+require 'pry'
+class School
+    def initialize(roster)
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        new_hash = { }
+        @roster.each do |grade, name|
+        new_hash[grade] = name.sort
+        end
+        new_hash
+    end
+
+end
